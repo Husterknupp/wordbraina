6db626efdd51453ba93ee84d7bd44edfeb826f48
Benjamin Schandera <Husterknupp@users.noreply.github.com>
2016-01-12 23:32:37 +0100

    update artifact version in batch file

--- wordbraina.bat
+++ wordbraina.bat
@@ -1,4 +1,4 @@
 @echo off
 set /p wordlength= Wie viele Buchstaben hat das Wort, das du suchst?: 
-"%JAVA_HOME%\bin\java.exe" -jar target\wordbraina-1.0-SNAPSHOT.jar %wordlength%
+"%JAVA_HOME%\bin\java.exe" -jar target\wordbraina-1.1-SNAPSHOT.jar %wordlength%
 PAUSE
