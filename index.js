4b1b4bbd03d6b17af809312b6792db4bbfb9c6fb
Husterknupp <4.23@gmx.de>
2016-02-09 21:48:07 +0100

    #1 hello world on heroku using node

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/', function(req, res) {
+  res.send('<h1>Hello, World!</h1>');
+});
+
+// todo can this be removed? Or is it used by heroku?
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
