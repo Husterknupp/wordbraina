68e26370bf06d84348ab10f9cf784fb81c74efa1
Benjamin Schandera <bens@spreadshirt.net>
2016-02-11 18:56:10 +0100

    #2 init matrix fields with value and position

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-var express = require('express');
-var app = express();
+var express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser'),
+    _ = require('underscore');
 
 app.set('port', (process.env.PORT || 5000));
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
     res.send('<h1>Hello, World!</h1>');
+});
+
+app.post('/puzzles', function (req, res) {
+    console.log(req.body.lines);
+    //var matrix = initMatrix(req.body.lines);
+    res.json(req.body);
+
+    //res.setHeader('Content-Type', 'application/json')
+    //res.write('you posted:\n');
+    //console.log(req.method);
+    //res.end(JSON.stringify(req.body, null, 2));
+    //res.end('d');
 });
 
 // todo can this be removed? Or is it used by heroku?
@@ -12,6 +27,20 @@ app.listen(app.get('port'), function () {
     console.log('Node app is running on port', app.get('port'));
 });
 
-exports.hello = function() {
-    return "world";
+exports.initMatrix = function (lines) {
+    var result = [];
+    var y = 0;
+    _.forEach(lines, function (line) {
+        var x = 0;
+        var newLine = [];
+        _.forEach(line, function (letter) {
+            if (letter != '') {
+                newLine.push({value: letter, neighbours: [], position: {x: x, y: y}});
+            }
+            x++;
+        });
+        result.push(newLine);
+        y++;
+    });
+    return result;
 };
--- spec/indexSpec.js
+++ spec/indexSpec.js
@@ -1,7 +1,17 @@
 var index = require('./../index');
 
-describe("index.hello", function () {
-    it("should say world", function () {
-        expect(index.hello()).toEqual("world");
-    });
+describe("index", function () {
+    describe("initMatrix", function () {
+        it("should instantiate fields properly", function () {
+            var expectedValuesWithNeighbours = [
+                [{ value: "a", neighbours: [], position: {x:0, y:0}},
+                    { value: "f", neighbours: [], position: {x:1, y:0}}],
+                [{ value: "e", neighbours: [], position: {x:1, y:1}} ]
+            ];
+            expect(index.initMatrix([
+                ["a", "f"],
+                ["", "e"]
+            ])).toEqual(expectedValuesWithNeighbours);
+        })
+    })
 });
