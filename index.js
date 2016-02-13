9fe97e38d96eb9110ebacb22a88c40a0114bc7f9
Benjamin Schandera <bens@spreadshirt.net>
2016-02-13 02:59:20 +0100

    #2 follow codacy.. avoid function inside loops+double quotes

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-var express = require('express'),
+var express = require("express"),
     app = express(),
-    bodyParser = require('body-parser'),
-    _ = require('underscore');
+    bodyParser = require("body-parser"),
+    _ = require("underscore");
 
-app.set('port', (process.env.PORT || 5000));
+app.set("port", (process.env.PORT || 5000));
 app.use(bodyParser.json());
 
 // todo can this be removed? Or is it used by heroku?
-app.listen(app.get('port'), function () {
-    console.log('Node app is running on port', app.get('port'));
+app.listen(app.get("port"), function () {
+    console.log("Node app is running on port", app.get("port"));
 });
 
-app.get('/', function (req, res) {
-    res.send('<h1>Hello, World!</h1>');
+app.get("/", function (req, res) {
+    res.send("<h1>Hello, World!</h1>");
 });
 
-app.post('/puzzles', function (req, res) {
-    console.log(req.body.lines);
+app.post("/puzzles", function (req, res) {
+    //req.body.lines;
     //var matrix = initMatrix(req.body.lines);
     res.write("matrix initialized and neighbours found");
 
@@ -47,44 +47,50 @@ exports.initMatrix = function (lines) {
     return {rows: rows, rowCount: y};
 };
 
+function findNeighbourInRow(row, x, y) {
+    return _.find(row, function (field) {
+        return field.x === x && field.y === y;
+    });
+}
+
+function addNeighbourIfPresent (matrix, field, neighbourX, neighbourY) {
+    for (var i = 0; i < matrix.rows.length; i++) {
+        var neighbour = findNeighbourInRow(matrix.rows[i], neighbourX, neighbourY);
+        if (typeof neighbour !== "undefined") {
+            field.neighbours.push(neighbour);
+        }
+    }
+}
+
+function addNeighboursDependingOnPosition(field, matrix) {
+    addNeighbourIfPresent(matrix, field, field.x + 1, field.y);
+    addNeighbourIfPresent(matrix, field, field.x, field.y + 1);
+    addNeighbourIfPresent(matrix, field, field.x + 1, field.y + 1);
+    if (field.x !== 0) {
+        // also fields to the left
+        addNeighbourIfPresent(matrix, field, field.x - 1, field.y);
+        addNeighbourIfPresent(matrix, field, field.x - 1, field.y + 1);
+    }
+    if (field.y !== 0) {
+        // also fields above
+        addNeighbourIfPresent(matrix, field, field.x + 1, field.y - 1);
+        addNeighbourIfPresent(matrix, field, field.x, field.y - 1);
+    }
+    if (field.x !== 0 && field.y !== 0) {
+        // also upper left corner
+        addNeighbourIfPresent(matrix, field, field.x - 1, field.y - 1);
+    }
+}
+
 // todo consider matrix as a class
 exports.initNeighbours = function (matrix) {
     for (var i = 0; i < matrix.rowCount; i++) {
         var j = 0;
         while (matrix.rows[i][j]) {
             var field = matrix.rows[i][j];
-            // only right and below
-            addNeighbourIfPresent(matrix, field, field.x + 1, field.y);
-            addNeighbourIfPresent(matrix, field, field.x, field.y + 1);
-            addNeighbourIfPresent(matrix, field, field.x + 1, field.y + 1);
-            if (field.x != 0) {
-                // also fields to the left
-                addNeighbourIfPresent(matrix, field, field.x - 1, field.y);
-                addNeighbourIfPresent(matrix, field, field.x - 1, field.y + 1);
-            }
-            if (field.y != 0) {
-                // also fields above
-                addNeighbourIfPresent(matrix, field, field.x + 1, field.y - 1);
-                addNeighbourIfPresent(matrix, field, field.x, field.y - 1);
-            }
-            if (field.x != 0 && field.y != 0) {
-                // also upper left corner
-                addNeighbourIfPresent(matrix, field, field.x - 1, field.y - 1);
-            }
+            addNeighboursDependingOnPosition(field, matrix);
             j++;
         }
     }
     return matrix;
 };
-
-function addNeighbourIfPresent (matrix, field, neighbourX, neighbourY) {
-    for (var i = 0; i < matrix.rows.length; i++) {
-        var row = matrix.rows[i];
-        var neighbour = _.find(row, function (field) {
-            return field.x === neighbourX && field.y === neighbourY;
-        });
-        if (typeof neighbour !== 'undefined') {
-            field.neighbours.push(neighbour);
-        }
-    }
-}
