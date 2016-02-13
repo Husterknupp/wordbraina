dfc699ad246fd655936edc0e28538a1057a03e6e
Benjamin Schandera <bens@spreadshirt.net>
2016-02-13 01:45:32 +0100

    #2 init neighbours
    also matrix.rowCount for easier traversion
    npm-test.sh for IntelliJ jasmine integration

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ var express = require('express'),
 app.set('port', (process.env.PORT || 5000));
 app.use(bodyParser.json());
 
+// todo can this be removed? Or is it used by heroku?
+app.listen(app.get('port'), function () {
+    console.log('Node app is running on port', app.get('port'));
+});
+
 app.get('/', function (req, res) {
     res.send('<h1>Hello, World!</h1>');
 });
@@ -13,8 +18,9 @@ app.get('/', function (req, res) {
 app.post('/puzzles', function (req, res) {
     console.log(req.body.lines);
     //var matrix = initMatrix(req.body.lines);
-    res.json(req.body);
+    res.write("matrix initialized and neighbours found");
 
+    //res.json(req.body);
     //res.setHeader('Content-Type', 'application/json')
     //res.write('you posted:\n');
     //console.log(req.method);
@@ -22,25 +28,63 @@ app.post('/puzzles', function (req, res) {
     //res.end('d');
 });
 
-// todo can this be removed? Or is it used by heroku?
-app.listen(app.get('port'), function () {
-    console.log('Node app is running on port', app.get('port'));
-});
-
 exports.initMatrix = function (lines) {
-    var result = [];
+    var rows = [];
     var y = 0;
     _.forEach(lines, function (line) {
         var x = 0;
         var newLine = [];
         _.forEach(line, function (letter) {
-            if (letter != '') {
-                newLine.push({value: letter, neighbours: [], position: {x: x, y: y}});
+            if (letter.trim() !== "") {
+                //newLine.push(new Field(letter.trim(), x, y));
+                newLine.push({value: letter.trim(), neighbours: [], x: x, y: y});
             }
             x++;
         });
-        result.push(newLine);
+        rows.push(newLine);
         y++;
     });
-    return result;
+    return {rows: rows, rowCount: y};
 };
+
+// todo consider matrix as a class
+exports.initNeighbours = function (matrix) {
+    for (var i = 0; i < matrix.rowCount; i++) {
+        var j = 0;
+        while (matrix.rows[i][j]) {
+            var field = matrix.rows[i][j];
+            // only right and below
+            addNeighbourIfPresent(matrix, field, field.x + 1, field.y);
+            addNeighbourIfPresent(matrix, field, field.x, field.y + 1);
+            addNeighbourIfPresent(matrix, field, field.x + 1, field.y + 1);
+            if (field.x != 0) {
+                // also fields to the left
+                addNeighbourIfPresent(matrix, field, field.x - 1, field.y);
+                addNeighbourIfPresent(matrix, field, field.x - 1, field.y + 1);
+            }
+            if (field.y != 0) {
+                // also fields above
+                addNeighbourIfPresent(matrix, field, field.x + 1, field.y - 1);
+                addNeighbourIfPresent(matrix, field, field.x, field.y - 1);
+            }
+            if (field.x != 0 && field.y != 0) {
+                // also upper left corner
+                addNeighbourIfPresent(matrix, field, field.x - 1, field.y - 1);
+            }
+            j++;
+        }
+    }
+    return matrix;
+};
+
+function addNeighbourIfPresent (matrix, field, neighbourX, neighbourY) {
+    for (var i = 0; i < matrix.rows.length; i++) {
+        var row = matrix.rows[i];
+        var neighbour = _.find(row, function (field) {
+            return field.x === neighbourX && field.y === neighbourY;
+        });
+        if (typeof neighbour !== 'undefined') {
+            field.neighbours.push(neighbour);
+        }
+    }
+}
--- spec/indexSpec.js
+++ spec/indexSpec.js
@@ -1,17 +1,105 @@
-var index = require('./../index');
+var index = require("./../index"),
+    _ = require("underscore");
 
 describe("index", function () {
     describe("initMatrix", function () {
-        it("should instantiate fields properly", function () {
-            var expectedValuesWithNeighbours = [
-                [{ value: "a", neighbours: [], position: {x:0, y:0}},
-                    { value: "f", neighbours: [], position: {x:1, y:0}}],
-                [{ value: "e", neighbours: [], position: {x:1, y:1}} ]
-            ];
+        it("should instantiate fields properly and ignore empty fields", function () {
+            var expectedValuesWithNeighbours = {
+                rows: [
+                    [{value: "a", neighbours: [], x: 0, y: 0},
+                        {value: "f", neighbours: [], x: 1, y: 0}],
+                    [{value: "e", neighbours: [], x: 1, y: 1}]
+                ], rowCount: 2
+            };
             expect(index.initMatrix([
                 ["a", "f"],
                 ["", "e"]
             ])).toEqual(expectedValuesWithNeighbours);
-        })
-    })
+        });
+
+        it("should ignore white spaces", function () {
+            var expectedValuesWithNeighbours = {
+                rows: [
+                    [{value: "f", neighbours: [], x: 1, y: 0}],
+                    [{value: "e", neighbours: [], x: 1, y: 1}]
+                ], rowCount: 2
+            };
+            expect(index.initMatrix([
+                [" ", "f"],
+                ["  ", "e"]
+            ])).toEqual(expectedValuesWithNeighbours);
+        });
+
+        it("should handle empty matrix", function () {
+            expect(index.initMatrix([])).toEqual({rows: [], rowCount: 0});
+        });
+    });
+
+    describe("initNeighbours", function () {
+        it("should put as neighbour each field left, right, below, above", function () {
+            var matrix = {
+                rows: [
+                    [{value: "a", neighbours: [], x: 0, y: 0},
+                        {value: "b", neighbours: [], x: 1, y: 0}],
+                    [{value: "c", neighbours: [], x: 0, y: 1},
+                        {value: "d", neighbours: [], x: 1, y: 1}]
+                ], rowCount: 2
+            };
+
+            var neighbours = index.initNeighbours(matrix);
+
+            var a = neighbours.rows[0][0];
+            var b = _.find(a.neighbours, function (field) {
+                return field.value === "b";
+            });
+            expect(b).toBeDefined();
+            var c = _.find(a.neighbours, function (field) {
+                return field.value === "c";
+            });
+            expect(c).toBeDefined();
+            var d = _.find(a.neighbours, function (field) {
+                return field.value === "d";
+            });
+            expect(d).toBeDefined();
+
+            var a1 = _.find(b.neighbours, function (field) {
+                return field.value === "a";
+            });
+            expect(a1).toBeDefined();
+            var c1 = _.find(b.neighbours, function (field) {
+                return field.value === "c";
+            });
+            expect(c1).toBeDefined();
+            var d1 = _.find(b.neighbours, function (field) {
+                return field.value === "d";
+            });
+            expect(d1).toBeDefined();
+        });
+        it("should not add field of next +1 row as neighbour", function () {
+            var matrix = {
+                rows: [
+                    [{value: "a", neighbours: [], x: 0, y: 0}],
+                    [],
+                    [{value: "c", neighbours: [], x: 0, y: 2}]
+                ], rowCount: 3
+            };
+
+            var neighbours = index.initNeighbours(matrix);
+            var a = neighbours.rows[0][0];
+            expect(a.neighbours).toEqual([]);
+        });
+
+        it("should not add field of same row, two right as neighbour", function () {
+            var matrix = {
+                rows: [
+                    [{value: "a", neighbours: [], x: 0, y: 0},{value: "a", neighbours: [], x: 0, y: 2}]
+                ], rowCount: 1
+            };
+
+            var neighbours = index.initNeighbours(matrix);
+            var a = neighbours.rows[0][0];
+            expect(a.neighbours).toEqual([]);
+        });
+
+    });
 });
