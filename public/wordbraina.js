329a247e427e6ce29f011aaa52c99b913c094eae
husterknupp <husterknupp@outlook.com>
2016-03-24 08:23:44 +0100

    #9 word finding works
    - angular to 1.5.2 from 1.3.15

--- public/wordbraina.js
+++ public/wordbraina.js
@@ -1,12 +1,37 @@
 angular.module('wordbraina', [])
-    .controller('puzzleController', function() {
+    .controller('puzzleController', ['$http', function($http) {
         var vm = this;
-        vm.puzzle = [];
+        vm.puzzle = "";
         vm.puzzleSplit = [];
         vm.words = [];
+        vm.wordLength = 4;
+
+        var separateCharacters = function(line) {
+            var result = [];
+            for (var i = 0; i < line.length; i++) {
+                result.push(line[i]);
+            }
+            return result;
+        };
 
         vm.findWords = function() {
-            vm.puzzleSplit = vm.puzzle.split("\n");
-            vm.words = vm.puzzleSplit;
+            var lines = [];
+            vm.puzzle.split("\n").forEach(function(line) {
+                lines.push(separateCharacters(line));
+            });
+
+            var puzzleId;
+            var payload = {lines: lines};
+            $http.post("/puzzles", payload).then(function(response) {
+                puzzleId = response.data;
+                $http.get("/puzzles/" + puzzleId + "/words?length=" + vm.wordLength).then(function(response) {
+                    vm.words = response.data;
+                }, function(response) {
+                    alert(":-/  GET didnt work. Here's what I got: " + response);
+                })
+            }, function(response) {
+                alert(":-/  POST didnt work. Here's what I got: " + response);
+            });
+
         };
-    });
+    }]);
