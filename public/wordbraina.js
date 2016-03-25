3520d4c00a4651ac7e6b48fa1e91e70f856655cf
husterknupp <husterknupp@outlook.com>
2016-03-26 00:48:31 +0100

    #9 spinning wheel and feedback when result is empty

--- public/wordbraina.js
+++ public/wordbraina.js
@@ -1,10 +1,12 @@
 angular.module('wordbraina', [])
-    .controller('puzzleController', ['$http', function($http) {
+    .controller('puzzleController', ['$http', '$timeout', function($http, $timeout) {
         var vm = this;
         vm.puzzle = "";
         vm.puzzleSplit = [];
         vm.words = [];
         vm.wordLength = 4;
+        vm.requestLoading = false;
+        vm.noWordsFound = false;
 
         var separateCharacters = function(line) {
             var result = [];
@@ -22,16 +24,25 @@ angular.module('wordbraina', [])
 
             var puzzleId;
             var payload = {lines: lines};
+            vm.requestLoading = true;
             $http.post("/puzzles", payload).then(function(response) {
                 puzzleId = response.data;
                 $http.get("/puzzles/" + puzzleId + "/words?length=" + vm.wordLength).then(function(response) {
+                    vm.requestLoading = false;
                     vm.words = response.data;
+                    if (vm.words.length <= 0) {
+                        vm.noWordsFound = true;
+                        $timeout(function() {
+                            vm.noWordsFound = false;
+                        }, 3500);
+                    }
                 }, function(response) {
+                    vm.requestLoading = false;
                     alert(":-/  GET didnt work. Here's what I got: " + response.data);
                 })
             }, function(response) {
+                vm.requestLoading = false;
                 alert(":-/  POST didnt work. Here's what I got: " + response.data);
             });
-
         };
     }]);
